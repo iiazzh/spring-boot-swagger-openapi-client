e305ceacb8082865e10f439e3853824aeb92b332
suhas <86886386+iiazzh@users.noreply.github.com>
2021-10-13 19:46:21 +0530

    First commit. Add basic project files.

---
+++ src/main/java/com/suhasmh/swaggeropenapiclient/SwaggeropenapiclientApplication.java
@@ -0,0 +1,13 @@
+package com.suhasmh.swaggeropenapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggeropenapiclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggeropenapiclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhasmh/swaggeropenapiclient/SwaggeropenapiclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhasmh.swaggeropenapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggeropenapiclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
